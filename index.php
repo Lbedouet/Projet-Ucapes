854df1ec6e1f1750d6ea115442b0fdfc119b0bff
Valantina80 <a.f.l@hotmail.fr>
2018-05-09 14:37:41 +0200

    index footer et header

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'includes/session_start.php';
+require_once('includes/db.php');
+require_once('includes/header.php');
+?>
+
+    <h2>Bienvenue sur mon super site !</h2>
+
+    <?php
+if (isset($_SESSION['utilisateur'])) { ?>
+        <h3>Vous êtes connecté en tant que
+            <?php echo $_SESSION['utilisateur']['email']; ?>
+        </h3>
+        <?php                                                              
+?>
+            <?php } else { ?> <a href="login.php">Lien de connexion</a>
+            <?php } ?>
+            <?php
+require_once('includes/footer.php');
+?>
