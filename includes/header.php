bf33e246e3d372e507d34a17ec9a729d57400989
Spootnika <celia.lflr@gmail.com>
2018-05-14 14:00:26 +0200

    Suppression du s de Ucape dans le title

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="../css/style.css">
-    <title>Projet Ucapes</title>
+    <title>Projet Ucape</title>
     
     <!---- mettre si compte admin ou compte éléve dans le header ------>
 </head>
