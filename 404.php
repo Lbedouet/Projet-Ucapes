a855970d57284ddde903f6be0404645639c99085
Unknown <lbedouet@protonmail.com>
2018-05-22 12:15:23 +0200

    Ajout de la page 404 (et non la 401 comme avant )
    Ajout d'une page 404 pour l'erreur 404

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'includes/session_start.php';
+require_once 'includes/db.php' ;
+require_once 'includes/header.php' ;
+?>
+
+   <center><h2>Erreur 404 : Page non trouve</h2><br/>
+   	<h3>La page que vous voulez accèder n'existe pas </h3>
+ 	<br/>
+ 	<h4> Retour vers l'accueil </h4>
+ 	<br/>
+<h2>   	<a href="index.php">Page d'accueil </a></h2>
+ 
+<?php
+require_once 'includes/footer.php' ;
+?>
